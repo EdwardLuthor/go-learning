ae9836ec007c0280de2982d9c04f965f8b377ffd
EdwardLuthor <alexandrobtway@gmail.com>
2025-04-19 06:00:02 +1000

    add solution for task1**

--- task1/main.go
+++ task1/main.go
@@ -19,20 +19,20 @@ func main() {
 	nums1 := []int{2, 7, 11, 15}
 	target1 := 9
 	result1 := twoSum(nums1, target1)
-	fmt.Printf("Input: nums = %v, target = %d, Output: %v\n", nums1, target1, result1)
+	fmt.Println("Input: nums =", nums1, ", target =", target1, ", Output:", result1)
 
 	nums2 := []int{3, 2, 4}
 	target2 := 6
 	result2 := twoSum(nums2, target2)
-	fmt.Printf("Input: nums = %v, target = %d, Output: %v\n", nums2, target2, result2)
+	fmt.Println("Input: nums =", nums2, ", target =", target2, ", Output:", result2)
 
 	nums3 := []int{3, 3}
 	target3 := 6
 	result3 := twoSum(nums3, target3)
-	fmt.Printf("Input: nums = %v, target = %d, Output: %v\n", nums3, target3, result3)
+	fmt.Println("Input: nums =", nums3, ", target =", target3, ", Output:", result3)
 
 	nums4 := []int{3, 3}
 	target4 := 9
 	result4 := twoSum(nums4, target4)
-	fmt.Printf("Input: nums = %v, target = %d, Output: %v\n", nums4, target4, result4)
+	fmt.Println("Input: nums =", nums4, ", target =", target4, ", Output:", result4)
 }
